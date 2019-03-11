ab7eaca6051deea448cb87c47c4d55d25078f593
quangduc97 <quangduc280497@gmail.com>
2019-03-12 01:19:17 +0700

    Upload code cua Hung

---
+++ ChuyenDoiDonVi/app/src/main/java/vn/edu/ptit/hcm/chuyendoidonvi/MainActivity.java
@@ -0,0 +1,20 @@
+package vn.edu.ptit.hcm.chuyendoidonvi;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*setContentView(R.layout.activity_main);*/
+        setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.weightList);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+    }
+}
